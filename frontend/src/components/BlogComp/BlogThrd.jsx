8c794b3201fe40373bf2e24751ef48dde3547f38
Suraj Yadav <surajyadavssr@gmail.com>
2024-06-17 16:34:49 +0530

    third component done now working on dynamic owlcarousel

--- frontend/src/components/BlogComp/BlogThrd.jsx
+++ frontend/src/components/BlogComp/BlogThrd.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "./BlogThrd.css";
+// import blogs from '../../assets/data/BlogPgTp.json';
+// import BlogTitle from "./BlogTitle";
+
+// function BlogThrd() {
+//   return (
+//     <div className="blg-thrd-mn-ctr">
+//       <div className='ti-tl-lg'>
+//       <BlogTitle className="hd-tp-rd" title="Explore Articles"/>
+//       </div>
+      
+//       {blogs.map((blog, index) => (
+//         <div key={blog.id} className="blg-thrd-sb-dv">
+//         <div className="ti-tl-lg">
+        
+//         </div>
+//           <div className={`gd-cnt ${blog.id % 2 === 0 ? "flipped" : ""}`}>
+//             <div className="gbsbfr-ctr">
+//               <h3>{blog.heading}</h3>
+//               <p>{blog.content}</p>
+              
+//               <Link to={`/blog/${blog.id}`} className="read-more-link">
+//                 <button type="button">Read more</button>
+//               </Link>
+//             </div>
+//             <div className="img-cntr">  
+//               <img src={`/images/${blog.img}`} alt={blog.title} />
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default BlogThrd;
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./BlogThrd.css";
-import blogs from '../../assets/data/BlogPgTp.json';
 import BlogTitle from "./BlogTitle";
 
 function BlogThrd() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/getBlogs")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data))
+      .catch((error) => console.error("Error fetching blogs:", error));
+  }, []);
+
   return (
     <div className="blg-thrd-mn-ctr">
       <div className='ti-tl-lg'>
@@ -13,12 +59,9 @@ function BlogThrd() {
       
       {blogs.map((blog, index) => (
         <div key={blog.id} className="blg-thrd-sb-dv">
-        <div className="ti-tl-lg">
-        
-        </div>
           <div className={`gd-cnt ${blog.id % 2 === 0 ? "flipped" : ""}`}>
             <div className="gbsbfr-ctr">
-              <h3>{blog.heading}</h3>
+              <h3>{blog.title}</h3>
               <p>{blog.content}</p>
               
               <Link to={`/blog/${blog.id}`} className="read-more-link">
@@ -26,7 +69,7 @@ function BlogThrd() {
               </Link>
             </div>
             <div className="img-cntr">  
-              <img src={`/images/${blog.img}`} alt={blog.title} />
+              <img src={`http://localhost:3001${blog.img}`} alt={blog.title} />
             </div>
           </div>
         </div>
@@ -36,3 +79,4 @@ function BlogThrd() {
 }
 
 export default BlogThrd;
+
